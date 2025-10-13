689cc7224893075f52a6170c33e42d2c9edd6311
Cyclization_z <1991009890@qq.com>
2025-10-13 17:59:08 -0400

    fix(db): default to MySQL classic protocol and add docker profile
    - Change datasource URL to use classic 3306 by default with env overrides (DB_HOST/DB_PORT/DB_NAME/DB_USER/DB_PASS)
    - Add application-docker.properties mapping host 33060 -> container 3306 for JDBC
    - Update SETUP.md to explain classic vs X Protocol and override examples
    - Harden mvnw Java version parsing (no more 'Illegal number' on Java 21)

--- proj2/mvnw.cmd
+++ proj2/mvnw.cmd
@@ -1,11 +1,31 @@
 @ECHO OFF
 SETLOCAL
-set MVN_CMD=mvn
+
 set WRAPPER_JAR=.mvn\wrapper\maven-wrapper.jar
 set WRAPPER_PROPS=.mvn\wrapper\maven-wrapper.properties
-IF NOT EXIST %WRAPPER_JAR% (
+
+for /f "tokens=2 delims==" %%A in ('findstr /R "^wrapperUrl=" "%WRAPPER_PROPS%"') do set WRAPPER_URL=%%A
+
+IF NOT EXIST "%WRAPPER_JAR%" (
   mkdir .mvn\wrapper 2> NUL
-  powershell -Command "Invoke-WebRequest -UseBasicParsing https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.3.2/maven-wrapper-3.3.2.jar -OutFile %WRAPPER_JAR%"
+  powershell -Command "Invoke-WebRequest -UseBasicParsing %WRAPPER_URL% -OutFile '%WRAPPER_JAR%'"
 )
-"%MVN_CMD%" %*
+
+set JAVA_EXE=java
+IF DEFINED JAVA_HOME set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+
+REM Find project base directory
+set MAVEN_PROJECTBASEDIR=%CD%
+if exist ".mvn" goto baseDirFound
+set DIR=%CD%
+:findBaseDir
+if exist "%DIR%\.mvn" set MAVEN_PROJECTBASEDIR=%DIR% & goto baseDirFound
+cd ..
+if "%DIR%"=="%CD%" goto baseDirFound
+set DIR=%CD%
+goto findBaseDir
+:baseDirFound
+
+"%JAVA_EXE%" -Dmaven.multiModuleProjectDirectory="%MAVEN_PROJECTBASEDIR%" -cp "%WRAPPER_JAR%" org.apache.maven.wrapper.MavenWrapperMain %*
+
 ENDLOCAL
