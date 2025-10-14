f69622cd3635d459f3904a39e68dde688a6ebf69
Matthew Nguyen <matthewnguyen203@gmail.com>
2025-10-13 20:07:39 -0400

    Checkstyle Fixes

--- proj2/src/main/java/com/boozebuddies/api/HealthController.java
+++ proj2/src/main/java/com/boozebuddies/api/HealthController.java
@@ -1,9 +1,9 @@
 package com.boozebuddies.api;
 
+import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Map;
 
 @RestController
 public class HealthController {
