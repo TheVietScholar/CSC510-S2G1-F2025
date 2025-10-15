68a57075139007f2de1ea7ffa81d99219ba529c0
Cyclization_z <1991009890@qq.com>
2025-10-14 20:14:43 -0400

    style(checkstyle): fix FinalClass and import group spacing
    - Mark BoozeBuddiesApplication as final per FinalClassCheck
    - Add blank line between java.* and org.* imports in HealthController per ImportOrderCheck

--- proj2/src/main/java/com/boozebuddies/BoozeBuddiesApplication.java
+++ proj2/src/main/java/com/boozebuddies/BoozeBuddiesApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoozeBuddiesApplication {
+public final class BoozeBuddiesApplication {
 	private BoozeBuddiesApplication() {
 		// Prevent instantiation
 	}
