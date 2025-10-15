9497dee9bb7fb0910f51a8710e3b32332f7ee790
Matthew Nguyen <142542765+TheVietScholar@users.noreply.github.com>
2025-10-14 21:26:04 -0400

    Changes to BoozeBuddiesApplication.java (#6)
    * fix(BoozeBuddiesApplications): change final class to public
    
    * fix(BoozeBuddiesApplication): change public constructor to protected
    
    ---------
    
    Co-authored-by: Cyclization_z <1991009890@qq.com>

--- proj2/src/main/java/com/boozebuddies/BoozeBuddiesApplication.java
+++ proj2/src/main/java/com/boozebuddies/BoozeBuddiesApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoozeBuddiesApplication {
-  protected BoozeBuddiesApplication() {
+public final class BoozeBuddiesApplication {
+  private BoozeBuddiesApplication() {
     // Prevent instantiation
   }
 
