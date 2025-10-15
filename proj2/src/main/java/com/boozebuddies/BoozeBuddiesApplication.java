7283435f17093f1c6bb0a91ae5737c195f4b1911
Cyclization_z <1991009890@qq.com>
2025-10-14 21:01:29 -0400

    fix(BoozeBuddiesApplications): change final class to public

--- proj2/src/main/java/com/boozebuddies/BoozeBuddiesApplication.java
+++ proj2/src/main/java/com/boozebuddies/BoozeBuddiesApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class BoozeBuddiesApplication {
-	private BoozeBuddiesApplication() {
+public class BoozeBuddiesApplication {
+	public BoozeBuddiesApplication() {
 		// Prevent instantiation
 	}
 
