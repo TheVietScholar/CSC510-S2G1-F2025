9f3e484da26b2e87cfd1f99129c7ccfcddc09cb9
Cyclization_z <1991009890@qq.com>
2025-10-14 20:02:57 -0400

    fix(checkstyle.xml):fix checkstyle violations

--- proj2/src/main/java/com/boozebuddies/BoozeBuddiesApplication.java
+++ proj2/src/main/java/com/boozebuddies/BoozeBuddiesApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoozeBuddiesApplication {
+  private BoozeBuddiesApplication() {
+    // Prevent instantiation
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(BoozeBuddiesApplication.class, args);
   }
