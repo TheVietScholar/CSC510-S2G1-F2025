b1dca40d90be1e85f74a34894e58bd6fda1e769a
Tiehang Zhang <zth.chem@gmail.com>
2025-10-18 16:17:00 -0400

    feat(repository): implement OrderRepository interface

--- proj2/src/main/java/com/boozebuddies/repository/OrderRepository.java
+++ proj2/src/main/java/com/boozebuddies/repository/OrderRepository.java
@@ -1,5 +1,17 @@
 package com.boozebuddies.repository;
 
-public class OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.boozebuddies.entity.Order;
+import com.boozebuddies.entity.OrderItem;
+import com.boozebuddies.model.OrderStatus;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
