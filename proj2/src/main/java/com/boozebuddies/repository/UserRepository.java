74f050f738733efac77f554548ad049ce62e3b02
rckemp2 <rckemp2@ncsu.edu>
2025-10-20 11:05:14 -0400

    Updated UserRepository to ignore EmailCase

--- proj2/src/main/java/com/boozebuddies/repository/UserRepository.java
+++ proj2/src/main/java/com/boozebuddies/repository/UserRepository.java
@@ -11,9 +11,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  Optional<User> findByEmail(String email);
-
-  boolean existsByEmail(String email);
+  Optional<User> findByEmailIgnoreCase(String email);
+  boolean existsByEmailIgnoreCase(String email);
 
   // Simple role filter (roles is ElementCollection<String>)
   @Query("SELECT u FROM User u JOIN u.roles r WHERE LOWER(r) = LOWER(:role)")
