80f34262932c39d60d6792a8f149ebc9d427616d
mknguye2 <mknguye2@ncsu.edu>
2025-11-03 18:56:42 -0500

    Completed fixing of Merchant Testing

--- proj2/src/test/java/com/boozebuddies/controller/MerchantControllerTest.java
+++ proj2/src/test/java/com/boozebuddies/controller/MerchantControllerTest.java
@@ -12,6 +12,7 @@ import com.boozebuddies.entity.Order;
 import com.boozebuddies.mapper.MerchantMapper;
 import com.boozebuddies.security.JwtAuthenticationFilter;
 import com.boozebuddies.service.MerchantService;
+import com.boozebuddies.service.PermissionService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalTime;
 import java.util.List;
@@ -50,6 +51,8 @@ class MerchantControllerTest {
   @MockBean private MerchantService merchantService;
 
   @MockBean private MerchantMapper merchantMapper;
+
+  @MockBean private PermissionService permissionService;
 
   private Merchant testMerchant;
   private MerchantDTO testMerchantDTO;
