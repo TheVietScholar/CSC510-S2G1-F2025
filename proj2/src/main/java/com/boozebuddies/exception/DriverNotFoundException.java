df1ee8670a20e9fd7eea8b71a5bb83b68fe48def
rckemp2 <rckemp2@ncsu.edu>
2025-11-05 13:20:27 -0500

    Finished commenting exceptions

--- proj2/src/main/java/com/boozebuddies/exception/DriverNotFoundException.java
+++ proj2/src/main/java/com/boozebuddies/exception/DriverNotFoundException.java
@@ -1,10 +1,24 @@
 package com.boozebuddies.exception;
 
+/**
+ * Exception thrown when a driver is not found.
+ */
 public class DriverNotFoundException extends RuntimeException {
+  /**
+   * Constructs a new DriverNotFoundException with the specified message.
+   *
+   * @param message the detail message
+   */
   public DriverNotFoundException(String message) {
     super(message);
   }
 
+  /**
+   * Constructs a new DriverNotFoundException with the specified message and cause.
+   *
+   * @param message the detail message
+   * @param cause the cause of the exception
+   */
   public DriverNotFoundException(String message, Throwable cause) {
     super(message, cause);
   }
--- proj2/src/main/java/com/boozebuddies/exception/InvalidCredentialsException.java
+++ proj2/src/main/java/com/boozebuddies/exception/InvalidCredentialsException.java
@@ -3,10 +3,21 @@ package com.boozebuddies.exception;
 /** Exception thrown when authentication fails due to invalid credentials. */
 public class InvalidCredentialsException extends RuntimeException {
 
+  /**
+   * Constructs a new InvalidCredentialsException with the specified message.
+   *
+   * @param message the detail message
+   */
   public InvalidCredentialsException(String message) {
     super(message);
   }
 
+  /**
+   * Constructs a new InvalidCredentialsException with the specified message and cause.
+   *
+   * @param message the detail message
+   * @param cause the cause of the exception
+   */
   public InvalidCredentialsException(String message, Throwable cause) {
     super(message, cause);
   }
