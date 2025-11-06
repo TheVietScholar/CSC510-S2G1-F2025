dfddf5661f9eaf68a52448935a4665b5213f979a
mknguye2 <mknguye2@ncsu.edu>
2025-11-06 02:58:00 -0500

    Spotless Apply

--- proj2/src/test/java/com/boozebuddies/service/implementation/OrderServiceImplTest.java
+++ proj2/src/test/java/com/boozebuddies/service/implementation/OrderServiceImplTest.java
@@ -2,7 +2,6 @@ package com.boozebuddies.service.implementation;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 import com.boozebuddies.entity.Delivery;
