0ce6bdffe6aaacca9a5b7d4799bd3caf27ba4f6c
mknguye2 <mknguye2@ncsu.edu>
2025-11-06 22:20:43 -0500

    Removed Duplicated Variables

--- proj2/src/main/java/com/boozebuddies/dto/UserDTO.java
+++ proj2/src/main/java/com/boozebuddies/dto/UserDTO.java
@@ -52,9 +52,6 @@ public class UserDTO {
   /** When the user account was last updated */
   private LocalDateTime updatedAt;
 
-  private Double latitude;
-  private Double longitude;
-
   /** The latitude coordinate of the user's location */
   private Double latitude;
 
